bb48f9d7b757512024a4e9635884c2f6020053df
tcazcode <tcazme01@gmail.com>
2019-08-24 18:17:47 +0700

    store

--- AppSolve/app/Http/Controllers/ViewController.php
+++ AppSolve/app/Http/Controllers/ViewController.php
@@ -23,7 +23,7 @@ class ViewController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
@@ -34,7 +34,7 @@ class ViewController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
