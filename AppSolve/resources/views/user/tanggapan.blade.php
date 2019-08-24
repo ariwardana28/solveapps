0108c49c9653497a52e20375007f9781983748df
ariwardana28 <36886965+ariwardana28@users.noreply.github.com>
2019-08-24 19:33:54 +0800

    t

--- AppSolve/resources/views/user/tanggapan.blade.php
+++ AppSolve/resources/views/user/tanggapan.blade.php
@@ -81,7 +81,7 @@
                 <nav class="sidebar-nav left-sidebar-menu-pro">
                     <ul class="metismenu" id="menu1">
                          <li>
-                            <a title="Landing Page" href="{{route('view.index')}}" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Home</span></a>
+                            <a title="Landing Page" href="{{url('/home')}}" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Home</span></a>
                         </li>
                         <li>
                             <a title="Landing Page" href="events.html" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Tanggapan</span></a>
